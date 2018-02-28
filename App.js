575c034015c297c94365450794b00de8dab102fa
Abigail McPhillips <abigail.mcphillips@gmail.com>
2018-02-28 13:39:05 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
---
+++ README.md
@@ -0,0 +1,12 @@
+# Particle SDK - React Native bridge
+
+A React Native integration for the [Particle iOS SDK](https://docs.particle.io/guide/getting-started/intro/photon/).
+
+Using this basic app as an example, you should be able to integrate the setup process of Particle-powered products into your own React Native app.
+
+## Running the sample app
+
+    $ git clone https://github.com/pixielabs/particle-react-native-bridge-example.git
+    $ cd particle-react-native-bridge-example
+    $ yarn
+    $ react-native run-ios
